7fc5c4626e0736f200b5a465861869990537746c
claudiadw <34047303+claudiadw@users.noreply.github.com>
2019-04-26 11:03:29 +0100

    Update time_test.py

--- RNR_ANALYSIS/time_test.py
+++ RNR_ANALYSIS/time_test.py
@@ -1,52 +1,59 @@
 import pickle
 import time
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 
 
+########## LOAD DATA
+
+bow_nofeats=pickle.load(open('bow_nofeats','rb'))
+bow_nofeats=pd.DataFrame(bow_nofeats.toarray())
+
+s_bow_matrix=pickle.load(open('selected_bow','rb'))
 
 csv = '/Users/cdw/Desktop/pkpd_script/RNR_ANALYSIS/ready_processed.csv'
 papers=pd.read_csv(csv)
 y = papers.loc[:,'category']
 
-bow_nofeats=pickle.load(open('bow_nofeats','rb'))
-bow_nofeats=pd.DataFrame(bow_nofeats.toarray())
-
-x_train_f,x_test_f,y_train_f,y_test_f=train_test_split(bow_nofeats,y,test_size=0.15,random_state=61097,stratify=y)
-
-print(bow_nofeats.shape[1])
-print('^^ shld be 42056\n')
 
 
-x_train_s,x_test_s,y_train_s,y_test_s=train_test_split(bow_nofeats,y,test_size=0.15,random_state=61097,stratify=y)
+######### TRAIN FULL SET 30 TIMES, FIND AV
 
-s_bow_matrix=pickle.load(open('selected_bow','rb'))
+time_f=[]
+for i in range(30):
+	x_train_f,x_test_f,y_train_f,y_test_f=train_test_split(bow_nofeats,y,test_size=0.15,stratify=y)
+	model1=XGBClassifier()
+	start_f = time.time()
+	model1.fit(x_train_f,y_train_f)
+	elapsed_f = time.time()-start_f
+	time_f.append(elapsed_f)
 
-print(s_bow_matrix.shape[1])
-print('^^ should be 55\n')
-
-
+av_time_f=np.mean(time_f)
+print(av_time_f)
 
 
 
-model1=XGBClassifier()
-start_t = time.time()
+###### TRAIN SMALLER SET 30 TIMES, FIND AV
 
-model1.fit(x_train_f,y_train_f)
+time_s=[]
+for  i in  range(30):
+	x_train_s,x_test_s,y_train_s,y_test_s=train_test_split(s_bow_matrix,y,test_size=0.15,stratify=y)
+	model_s=XGBClassifier()
+	start_s = time.time()
+	model_s.fit(x_train_s,y_train_s)
+	elapsed_s=time.time()-start_s
+	time_s.append(elapsed_s)
 
-elapsed_t = time.time()-start_t
-print('full: {}s'.format(elapsed_t))
-print()
+av_time_s=np.mean(time_s)
+print(av_time_s)
 
 
 
 
+######## calculate difference in time.
 
-model2=XGBClassifier()
-start_s = time.time()
-
-model2.fit(x_train_s,y_train_s)
-
-elapsed_s=time.time()-start_x
-print('selected: {}s'.format(elapsed_s))
+dif = av_time_f-av_time_s
+reduction = dif/av_time_f*100
+print('reduction: {}%'.format(round(reduction)))
